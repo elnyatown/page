500bebb32455205af77277b142051f841ca6d510
Davinchi <support@elnyatown.ru>
2013-09-29 03:22:28 +0400

    bootstrap

--- index.php
+++ index.php
@@ -1,7 +1,65 @@
-<?php
 
-echo ("TEST OK");
+<!DOCTYPE HTML PUBLIC  "-//W3C//DTD HTML 4.01//EN" "www.w3.org/TR/html4/strict.dtd">
+<html>
+  <head>
+  <meta charset="utf-8">
+    <title>Шаблон Bootstrap 101</title>
+    <!-- Bootstrap -->
+    <link href="bootstrap/css/bootstrap.css" rel="stylesheet">
+	<link href="bootstrap/css/bootstrap-responsive.css">
+	<script src="jq.js"></script>
+    
+	
+	<script src="/bootstrap/js/bootstrap.js"></script>
+
+  </head>
+ <body>
 
 
 
-?>
+
+  <div class="container">
+
+ <div class="row">
+  <div class="span6">.xcvbx 88888 88888 8888 8888 cvb..</div>
+  <div class="span4 offset2">.
+  
+  <form class="form-horizontal">
+  <div class="control-group">
+    <label class="control-label" for="inputEmail">Email</label>
+    <div class="controls">
+      <input type="text" id="inputEmail" placeholder="Email">
+    </div>
+  </div>
+  <div class="control-group">
+    <label class="control-label" for="inputPassword">Password</label>
+    <div class="controls">
+      <input type="password" id="inputPassword" placeholder="Password">
+    </div>
+  </div>
+  <div class="control-group">
+    <div class="controls">
+      <label class="checkbox">
+        <input type="checkbox"> Remember me
+      </label>
+      <button type="submit" class="btn">Sign in</button>
+    </div>
+  </div>
+</form>
+  
+  
+  
+  
+  
+  
+  
+  .xcv44 444 444444 44444 4444 444b xcvb.</div>
+</div>
+	
+	
+	
+	
+	
+	</div>
+	</body>
+<html>

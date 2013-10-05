5a55531f73a35442193e956937da81503e60dda0
Davinchi <elnyatown@gmail.com>
2013-10-05 23:50:27 +0400

    дизайн на bootstrap

--- test.php
+++ test.php
@@ -1,30 +1,3 @@
-<?php
-date_default_timezone_set( 'Europe/Moscow' );
-
-
-include("kernel.class.php");
-$test=new kernel();
-$input["html"]=file_get_contents("global.template.tpl");
-//$input["html"]="sdfghf  dfghdj {mod_mainmenu}  ghjfghjfg fghjfghj g  {mod_content:var1=TEST1,var2=TEST2} ehjhf ekjhf{mod_submenu:g1=D1,g2=D2} ekjrhf";
-//var_dump($input["html"]);
-$out=$test->getListModules($input);
-
-
-//print_r($out["arrayListModulesTpl"]);
-
-
-
-
-
-
-
-
-//echo("test");
-?>
-
-
-
-
 <!DOCTYPE HTML PUBLIC  "-//W3C//DTD HTML 4.01//EN" "www.w3.org/TR/html4/strict.dtd">
 <!DOCTYPE html>
 <html>
@@ -45,21 +18,72 @@ $out=$test->getListModules($input);
 	
   </head>
   <body>
-  <div class="container">
+
+
+
+
+
+  <div class="container ">
   <div class="row">
+<div class="col-md-5 col-sm-5">
+<h4>ELNYATOWN ENGENE</h4>
+
+</div>
+
+
+<div class="col-md-7 col-sm-7">
   <ul class="nav nav-pills">
   <li class="active"><a href="#">Home</a></li>
   <li><a href="#">Profile</a></li>
   <li><a href="#">Messages</a></li>
 </ul>
   </div>
-  
+</div>
+  	
+
+
+<div class="row">
+  <div class="col-md-12 col-sm-12">
+    <a href="#" class="thumbnail">
+      <img src="header-bg.gif" alt="..." class="img-rounded img-responsive">
+    </a>
+  </div>
+</div>
+
+
+
+<div class="row">
+		<div class="col-md-12 col-sm-12">
+<ul class="breadcrumb">
+  <li><a href="#">Home</a></li>
+  <li><a href="#">Library</a></li>
+  <li class="active">Data</li>
+</ul>
+		</div>
+	</div>
+
+
   <div class="row">
-  <div class="col-md-9">10</div>
-  <div class="col-md-3">2
+  <div class="col-md-9 col-sm-9">10
+
+ЕЛЬНИНСКАЯ ОПЕРАЦИЯ
+Г. К. Жуков,Маршал Советского Союза
+
+В штабе фронта задержался ненадолго. Ознакомившись с последними событиями на участках фронта, вместе с командующим артиллерией генералом Л. А. Говоровым отправился в район Ельни, в штаб 24–й армии. Приехали туда поздно вечером. По дороге видели большие зарева пожаров где–то в районе Ярцева и Ельни.
+
+Что горело – неизвестно, но вид пожарищ производил тяжкое впечатление. Гибло в огне народное добро, многолетний труд советских людей. Я спрашивал себя: как и чем должен ответить врагу советский народ за беды, которые фашисты сеют на своем кровавом пути? Мечом, и только мечом, беспощадно уничтожая злобного врага,– был единственный ответ...
+
+В штабе 24–й армии нас встретил командарм К. И. Ракутин и командующие родами войск. К. И. Ра–кутина я раньше не знал. Докладывая дислокацию, он произвел на меня хорошее впечатление, но его оперативно–тактическая подготовка была явно недостаточной. Ему был присущ тот же недостаток, что офицерам и генералам, работавшим ранее в пограничных войсках Наркомата внутренних дел, где им почти не приходилось совершенствоваться в вопросах оперативного искусства.
+
+Рано утром следующего дня вместе с К. И. Ракути–ным мы выехали в район Ельни для личной рекогносцировки. В это время там шел огневой бой с противником. После обсуждения обстановки с командирами частей мы убедились, что немецкие войска здесь хорошо организовали оборону и, видимо, будут драться крепко. На переднем крае обороны и в ее глубине противник закопал в землю танки, штурмовые и артиллерийские орудия и тем самым превратил ельнинский выступ в своеобразный укрепленный район.
+
+
+
+</div>
+  <div class="col-md-3 col-sm-3">2
   <ul class="nav nav-pills nav-stacked">
-	<li class="active"><a href="#">Home</a></li>
-	<li><a href="#">Profile</a></li>
+	<li ><a href="#">Home</a></li>
+	<li class="active"><a href="#">Profile sjkhgf  ksdgfksjdh ksjh dgsjhd ljh gskdljhg skdjhg flksjdhgshd kfjg </a></li>
 	<li><a href="#">Messages</a></li>
 	<li class="dropdown">
 		<a class="dropdown-toggle" data-toggle="dropdown" href="#">
@@ -68,6 +92,7 @@ $out=$test->getListModules($input);
 		<ul class="dropdown-menu">
 			<li><a href="#">Profile</a></li>
 			<li><a href="#">Messages ert  ert er t 3q4t qretertewrt wert wer tewr t</a></li>
+			<li role="presentation" class="disabled"><a role="menuitem" tabindex="-1" href="#">Disabled link</a></li>
 		</ul>
   </li>
   
@@ -79,6 +104,33 @@ $out=$test->getListModules($input);
 </div>
 
 
+ <!-- Button trigger modal -->
+  <a data-toggle="modal" href="#myModal" class="btn btn-primary btn-lg">Launch demo modal</a>
+
+  <!-- Modal -->
+  <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+          <h4 class="modal-title">Modal title</h4>
+        </div>
+        <div class="modal-body">
+        TEST MODAL
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+          <button type="button" class="btn btn-primary">Save changes</button>
+        </div>
+      </div><!-- /.modal-content -->
+    </div><!-- /.modal-dialog -->
+  </div><!-- /.modal -->
+
+
+
 </div>
   </body>
 </html>
+
+
+
